cdce8b2743f58d0c3fc485fea7ebe26bff1573bb
TianYi <142967939+ztygod@users.noreply.github.com>
2025-04-17 22:00:37 +0800

    feat:不知道自己在写什么

--- src/fetcher/client.rs
+++
@@ -1,9 +0,0 @@
-use anyhow::Result;
-use reqwest::Client;
-
-pub async fn fetch_url(url: &str) -> Result<String> {
-    let client = Client::new();
-    let res = client.get(url).send().await?;
-    let body = res.text().await?;
-    Ok(body)
-}
