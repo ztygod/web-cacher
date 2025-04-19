98e5b9bae208fd2e098b11f2d01ee8733630fe09
TianYi <142967939+ztygod@users.noreply.github.com>
2025-04-19 16:12:01 +0800

    feat: metrics指标

--- src/scheduler/mod.rs
+++ src/scheduler/mod.rs
@@ -1,15 +1,1 @@
-mod metrics;
-
-use crate::{config::Config, fetcher};
-use anyhow::Result;
-use metrics::Metrics;
-use tokio::time::Duration;
-use tracing::debug;
-
-pub struct Scheduler {
-    config: Config,
-    metrics: Metrics,
-    interval: Duration,
-}
-
-impl Scheduler {}
+pub mod metrics;
